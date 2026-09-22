2b2fcafb5a829809261b22a2a7a165d591fed0c5
sslaquerre07 <sslaquerre@gmail.com>
2026-09-22 22:31:27 +0000

    Convert config file to JS for action

--- services/web/jest.config.js
+++ services/web/jest.config.js
@@ -1,12 +1,11 @@
 import nextJest from 'next/jest.js'
-import type { Config } from 'jest'
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
 
-const config: Config = {
+const config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom', // Simulates a browser environment for React components
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Used to load extra matchers like jest-dom
